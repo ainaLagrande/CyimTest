9465476c5191cbe45ffd38bf1dc10e6c74871d4d
aina200 <ainapukipuki@gmail.com>
2022-07-14 19:05:26 +0200

    Responsive mobil added

--- js/app.js
+++ js/app.js
@@ -15,3 +15,27 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";  
 }
+
+
+// Reveal on scroll 
+const threshold = .1
+const options = {
+	root:null,
+	rootMargin:'0px',
+	threshold
+}
+
+const handleIntersect = function (entries, observer) {
+	entries.forEach(function (entry) {
+	  if (entry.intersectionRatio > threshold) {
+			entry.target.classList.add('reveal-visible');
+			observer.unobserve(entry.target);
+		} 
+	});
+}
+const observer = new IntersectionObserver(handleIntersect ,options)
+document.querySelectorAll('.reveal').forEach(function(r){
+
+	observer.observe(r)
+
+})
